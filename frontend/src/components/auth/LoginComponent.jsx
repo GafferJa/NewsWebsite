25cebdef03a71a61a0ac926c60cb38fd0b9de5cd
GafferJa <abint987654321@gmail.com>
2024-03-26 09:31:04 +0545

    login updadate

--- frontend/src/components/auth/LoginComponent.jsx
+++ frontend/src/components/auth/LoginComponent.jsx
@@ -1,22 +1,44 @@
-import React from 'react';
-import HeaderComponent from '../../layouts/HeaderComponent';
-import FooterComponent from '../../layouts/FooterComponent';
+import React from 'react'
+import HeaderComponent from '../../layouts/HeaderComponent'
+import FooterComponent from '../../layouts/FooterComponent'
+import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+const loginSchema = yup.object().shape({
+    email: yup.string().email().required(),
+    password: yup.string().required(),
+});
 
 export default function LoginComponent() {
+    const { register, setError, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(loginSchema)
+    });
+
     return (
         <div>
-            <div className="container ">
+            <HeaderComponent />
+            <div className='container mt-3'>
                 <div className="row">
-                    <div className="col-md-12">
-                        <HeaderComponent />
-                    </div>
                     <div className="col-md-12">
                         <h1>Login Page</h1>
                     </div>
-                    <div className="col-md-12">
-
+                    <div className='col-md-12'>
+                        <form>
+                            <div className="form-group mb-2">
+                                <label htmlFor="">Email</label>
+                                <input type="email" className='form-control' name='email' />
+                            </div>
+                            <div className="form-group mb-2">
+                                <label htmlFor="">Password</label>
+                                <input type="password" className='form-control' name='password' />
+                            </div>
+                            <div className="form-group">
+                                <button className='btn btn-success'>Login</button>
+                            </div>
+                        </form>
                     </div>
-                    <div className="col-md-12">
+                    <div className="col-md-12 text-center mt-4">
                         <FooterComponent />
                     </div>
                 </div>
